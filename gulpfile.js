8cfbc9006ae09f1ec91a605f2d724502ed619116
otecMuxah <suxx_2000@hotmail.com>
2017-07-26 12:28:41 +0300

    case page

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ const gulp            = require('gulp');
 const path            = require('path');
 const sass            = require('gulp-sass');
 const autoprefixer    = require('gulp-autoprefixer');
+const concat          = require('gulp-concat');
+const minify          = require('gulp-minify');
+const cleanCss        = require('gulp-clean-css');
+const rev             = require('gulp-rev');
+const gzip            = require('gulp-gzip');
 
 const root      = path.resolve(__dirname);
 const src       = path.resolve(root, 'scss');
-const dest      = path.resolve(root, 'statics');
+const dest      = path.resolve(root, '');
 
 const config = {};
 
@@ -28,3 +33,26 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
     gulp.watch(config.styles.src, ['sass']);
 });
+
+
+
+gulp.task('pack-js', function () {
+    return gulp.src(['js/*.js', '!js/bundle.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('pack-css', function () {
+    return gulp.src(['css/custom/*.css', 'css/main.css'])
+        .pipe(concat('stylesheet.css'))
+        .pipe(cleanCss())
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('build', ['pack-css', 'pack-js']);
