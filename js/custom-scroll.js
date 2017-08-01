fd153595174eb3d2af278357928b3ca96c43336a
otecMuxah <suxx_2000@hotmail.com>
2017-08-01 17:03:47 +0300

    upd

--- js/custom-scroll.js
+++ js/custom-scroll.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var scrolledHeight = 0;
     var blocks = $('.block');
     blocks[0].className += ' active';
+    var block1 = $('.block1');
 
     //calculate total height of all blocks
     var totalHeight = function() {
@@ -15,10 +16,25 @@ document.addEventListener("DOMContentLoaded", function(){
         $('.wrapper').css('height',result);
     };
 
+    // function that allow event to happen in specific period of time
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+
     window.onresize = function () {
         totalHeight();
     };
-
     window.onscroll = function() {
         var active = $('.active');
         console.log(window.pageYOffset);
@@ -29,11 +45,14 @@ document.addEventListener("DOMContentLoaded", function(){
         if (!window.pageYOffset && !document.documentElement.scrollTop) {
             scrolledHeight = 0;
             scrolled = 0;
+            console.log("nulled");
+            blocks.removeClass('active');
+            block1.addClass('active');
         }
         if (rect.bottom < 0 ) {
             var nextSibl = active.next('.block');
             scrolledHeight += $('.active')[0].offsetHeight;
-            $('.active')[0].style.transform = 'translate(0,0)';
+            // blocks.css('transform', 'translate(0,0)');
             if (nextSibl.hasClass('block')) {
                 blocks.removeClass('active');
                 nextSibl.addClass('active');
@@ -44,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(scrolled);
             var prevSibling = active.prev('.block');
             $('.active')[0].style.transform = 'translate(0,0)';
+            //blocks.css('transform', 'translate(0,0)');
             if (prevSibling.hasClass('block')) {
                 blocks.removeClass('active');
                 prevSibling.addClass('active');
@@ -53,10 +73,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
         } else if (scrolled < 0 && $('.block1.active').length > 0) {
             blocks.removeClass('active');
-            $('.block1').addClass('active');
+            block1.addClass('active');
         }
 
         active[0].style.transform = translate;
     };
+
+
     totalHeight();
+    block1.style.transform = 'translate3d(0,0,0)';
+
 });
