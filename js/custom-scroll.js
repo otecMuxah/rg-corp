175f5b690ec52ece4b1846b5934b6af73b660e6f
Misha <suxx_2000@hotmail.com>
2017-08-14 02:31:59 +0300

    upd

--- js/custom-scroll.js
+++ js/custom-scroll.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             active[0].style.transform = translate;
-        }, 10); // 1 is number in ms how often function can be executed
+        }, 5); // 1 is number in ms how often function can be executed
 
         blocks[0].className += ' active';
 
