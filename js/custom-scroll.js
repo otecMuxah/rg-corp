95e64812a45f2e7e79ab5b10d1d6c503d9a59350
otecMuxah <suxx_2000@hotmail.com>
2017-09-04 14:19:09 +0300

    opacity and slide

--- js/custom-scroll.js
+++ js/custom-scroll.js
@@ -24,7 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     }
     if (blocks.length > 0) {
-        //calculate total height of all blocks
+       
+        //function to calculate total height of all blocks
         var totalHeight = function () {
             result = 0;
             for (var i = 0; i < blocks.length; i++) {
@@ -35,14 +36,18 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             wrapper.css('height', result);
         };
+         //calculate total height of all blocks
         totalHeight();
+        //debounced scroll function
         var scrollEffect = debounce(function () {
-            var scrolled = window.pageYOffset || document.documentElement.scrollTop,
-                scrolledBlock = Math.abs(scrolled - scrolledHeight),
+            var scrolled = window.pageYOffset || document.documentElement.scrollTop, // pixels scrolled from top of document
+                scrolledBlock = Math.abs(scrolled - scrolledHeight), 
                 translate = 'translate3d(0,-' + scrolledBlock + 'px,0)',
                 active = $('.active'),
+                viewportHeight = window.innerHeight,
                 currentBlockHeight = blocksObj[activeBlock] - (blocksObj[activeBlock - 1] || 0);
-
+            var nextBlock = $('.active').next('.block');
+            //Condition to switch to next block
             if (scrolled >= blocksObj[activeBlock]) {
                 scrolledHeight = blocksObj[activeBlock];
                 translate = 'translate3d(0,-' + currentBlockHeight + 'px,0)';
@@ -50,7 +55,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 activeBlock++;
                 blocks.removeClass('active');
                 $('.block' + activeBlock).addClass('active');
-            } else if (scrolled <= scrolledHeight && activeBlock && scrolled < blocksObj[activeBlock - 1]) {
+                
+            } else if (scrolled <= scrolledHeight && activeBlock && scrolled < blocksObj[activeBlock - 1]) { //Condition to switch to prev block
                 translate = 'translate3d(0,0,0)';
                 active[0].style.transform = 'translate3d(0,0,0)';
                 scrolledHeight = blocksObj[activeBlock - 2];
@@ -63,9 +69,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 blocks.removeClass('active');
                 $('.block' + activeBlock).addClass('active');
             }
+            //opacity implementation
+            if (nextBlock.hasClass('js-opacity') && (currentBlockHeight - scrolledBlock) < viewportHeight) {
+                var percentScrolled = (currentBlockHeight - scrolledBlock)/viewportHeight;
+                $('.js-opacity-layer').css('opacity', percentScrolled);
+                if (percentScrolled <= 0) {
 
+                }
+            }
+            //sliding implementation
+            if (nextBlock.hasClass('js-sliding') && (currentBlockHeight - scrolledBlock) < viewportHeight) {
+                var percentScrolled = (currentBlockHeight - scrolledBlock)/viewportHeight;
+                nextBlock.find('.solutions-banner_image-wrapper').css('transform','translate3d(-' + percentScrolled*100 + '%,0,0)');
+                nextBlock.find('.monitor-block_text-wrapper').css('transform','translate3d(' + percentScrolled*100 + '%,0,0)');
+            }
+            
             active[0].style.transform = translate;
-        }, 1); // 1 is number in ms how often function can be executed
+        }, 10); // 1 is number in ms how often function can be executed
 
         blocks[0].className += ' active';
 
